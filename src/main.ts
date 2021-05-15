291b56ed9aac966e6e4a613006e5f83010e680cb
Hiroya Kubo <hiroya@cuc.ac.jp>
2021-05-15 23:54:34 +0900

    export function getOAuthToken to global

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import "./functions/form";
 import "./functions/onOpen";
 
 import { onOpen } from "./functions/onOpen";
+import {getOAuthToken} from './functions/picker/picker';
 global.onOpen = onOpen;
-
+global.getOAuthToken = getOAuthToken;
